7c425ea1a0070661c0eb1aeb225a58bec1d2b3fa
Nathaniel Woodthorpe <d12@github.com>
2020-02-26 14:39:20 -0500

    First version

---
+++ src/main/java/br/unicap/nomedisciplina/atividade00/Hello.java
@@ -0,0 +1,12 @@
+package br.unicap.nomedisciplina.atividade00;
+
+public class Hello {
+
+	static String messageImplemented() {
+		return "Hello World";
+	}
+
+	static String messageNotImplemented() {
+		throw new UnsupportedOperationException();
+	}
+}
